f13788e31760abaa8295a193923141596a964b40
kwantke <kokk7310@gamil.com>
2022-04-17 17:23:05 +0900

    알고리즘 추가

---
+++ TEST/AlgTest/src/Test10_nQueen.java
@@ -0,0 +1,93 @@
+import java.util.Scanner;
+
+public class Test10_nQueen {
+
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+
+    }
+}
+
+/*
+import java.util.Scanner;
+
+public class Test10_nQueen {
+
+    public static int[] arr;
+    public static int N;
+    public static int count = 0;
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        N = in.nextInt();
+        arr = new int[N];
+
+        nQueen(0);
+        System.out.println(count);
+
+    }
+
+    public static void nQueen(int depth) {
+        // 모든 원소를 다 채운 상태면 count 증가 및 return
+        if (depth == N) {
+            count++;
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            arr[depth] = i;
+            // 놓을 수 있는 위치일 경우 재귀호출
+            if (Possibility(depth)) {
+                nQueen(depth + 1);
+            }
+        }
+
+    }
+
+    public static boolean Possibility(int col) {
+
+        for (int i = 0; i < col; i++) {
+            // 해당 열의 행과 i열의 행이 일치할경우 (같은 행에 존재할 경우)
+            if (arr[col] == arr[i]) {
+                return false;
+            }
+
+            */
+/*
+             * 대각선상에 놓여있는 경우
+             * (열의 차와 행의 차가 같을 경우가 대각선에 놓여있는 경우다)
+             *//*
+
+            else if (Math.abs(col - i) == Math.abs(arr[col] - arr[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}*/
+
+
+/*
+*N-Queen
+문제
+N-Queen 문제는 크기가 N × N인 체스판 위에 퀸 N개를 서로 공격할 수 없게 놓는 문제이다.
+
+N이 주어졌을 때, 퀸을 놓는 방법의 수를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 N이 주어진다. (1 ≤ N < 15)
+
+출력
+첫째 줄에 퀸 N개를 서로 공격할 수 없게 놓는 경우의 수를 출력한다.
+
+예제 입력 1
+8
+예제 출력 1
+92
+*/
